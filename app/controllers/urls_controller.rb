c91698faf914288c3707003ea871caef0a1f09b8
sofiaadam <sofiaadamkho@gmail.com>
2017-04-25 13:28:36 +0800

    can create/display shorten links

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -5,8 +5,28 @@ class UrlsController < ApplicationController
     end
 
     def show
-    @url = Url.find(params[:id])
+        @url = Url.find(params[:id])
     end
 
+    def new
+        @url = Url.new
+    end
+
+    def create
+    @url = Url.new(url_params)
+    @url.shortener
+        if @url.save
+            redirect_to @url  
+        else
+            render 'new'
+        end   
+    end
+    
+
+    private
+    def url_params
+        params.require(:url).permit(:long_url)
+    end
+    
 
 end
